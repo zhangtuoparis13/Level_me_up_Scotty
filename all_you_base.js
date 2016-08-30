a82cd3487ba1c35963f3359c11d83e0cee65e88c
TBtuo <tuo.zhang@telecom-bretagne.eu>
2016-08-30 10:18:22 +0200

    all you base

--- all_you_base.js
+++ all_you_base.js
@@ -1,3 +1,16 @@
 /**
  * Created by TBtuo on 30/08/16.
  */
+
+/*
+let X = process.argv[2];
+let Y = process.argv[3];
+
+console.log(`All YOUR ${X} ARE BELONG TO ${Y}`);*/
+
+// console.log(
+//     'ALL YOUR %s ARE BELONG TO %s',
+//     process.argv[2],
+//     process.argv[3])
+
+console.log(`All YOUR ${process.argv[2]} ARE BELONG TO ${process.argv[3]}`);
